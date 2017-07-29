b746b031d1c9f3a2c93a1345fe500ac26e10b8b3
Finn Woelm <public@finnwoelm.com>
2017-07-29 10:14:13 +0700

    Set up basic devise configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.action_mailer.default_url_options = { host: 'upshift.one' }
 
   # Use a real queuing backend for Active Job (and separate queues per
   # environment)
