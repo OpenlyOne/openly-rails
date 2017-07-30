98eaccc78639dd3484d7d15c8ad4581e0f703048
Finn Woelm <public@finnwoelm.com>
2017-07-30 09:49:23 +0700

    Set up Capistrano configuration

---
+++ config/deploy.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+# config valid only for current version of Capistrano
+lock '3.9.0'
+
+server 'upshift.one', port: 22, roles: %i[web app db], primary: true
+
+set :repo_url,        'git@github.com:UpshiftOne/upshift.git'
+set :application,     'upshift'
+set :user,            ENV['DEPLOY_USER']
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/var/apps/#{fetch(:application)}"
+set :puma_bind,
+    "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,
+    forward_agent:  true,
+    user:           fetch(:user),
+    keys:           %w[~/.ssh/id_rsa.pub]
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,
+#    %w[bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc 'Make sure local git is in sync with remote.'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        abort 'Error: HEAD is not the same as origin/master'
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
