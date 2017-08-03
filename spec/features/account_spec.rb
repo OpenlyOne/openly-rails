3efe889b53416c982c7bf4695ab5178684d92dcc
Finn Woelm <public@finnwoelm.com>
2017-08-03 20:18:43 +0700

    Add navigation menu to navbar and footer
    On desktop devices, navigation menu can be found in navbar. On mobile
    devices, the menu is in the footer.

--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -2,10 +2,14 @@
 
 feature 'Account' do
   scenario 'User can create an account' do
-    # given I am on the sign up page
-    visit '/join'
+    # given I am on the homepage
+    visit '/'
 
-    # when I enter my email address and password
+    # when I click 'Join'
+    within 'nav' do
+      click_on 'Join'
+    end
+    # and enter my email address and password
     account = build(:account)
     within 'form' do
       fill_in 'Email', with: account.email
@@ -16,7 +20,6 @@ feature 'Account' do
 
     # then I should see a success message
     expect(page).to have_text 'signed up successfully'
-
     # and there should be an account in the database
     expect(Account.count).to equal 1
     expect(Account).to exist(email: account.email)
@@ -26,9 +29,13 @@ feature 'Account' do
     # given I am a signed-in user
     account = create(:account)
     sign_in account, scope: :account
+    # and I am on the homepage
+    visit '/'
 
     # when I go to the edit account page
-    visit '/account'
+    within 'nav' do
+      click_on 'Account'
+    end
     # and update my password
     fill_in 'Current password', with: account.password
     fill_in 'Password', with: 'newpassword', match: :first
@@ -37,10 +44,8 @@ feature 'Account' do
 
     # then I should be (remain) on the account page
     expect(page).to have_current_path '/account'
-
     # and see a success message
     expect(page).to have_text 'updated successfully'
-
     # and my password should be updated in the database
     expect(account.reload).to be_valid_password('newpassword')
   end
@@ -49,15 +54,18 @@ feature 'Account' do
     # given I am a signed-in user
     account = create(:account)
     sign_in account, scope: :account
+    # and I am on the homepage
+    visit '/'
 
     # when I go to the edit account page
-    visit '/account'
+    within 'nav' do
+      click_on 'Account'
+    end
     # and click cancel my account
     click_on 'Delete my account'
 
     # then I should see a success message
     expect(page).to have_text 'successfully cancelled'
-
     # and my account should be deleted from the database
     expect(Account.count).to equal 0
     expect(Account).not_to exist(email: account.email)
--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -4,9 +4,13 @@ feature 'Session' do
   scenario 'User can log in' do
     # given I have an account
     account = create(:account)
+    # and I am on the homepage
+    visit '/'
 
-    # when I visit the login page
-    visit '/login'
+    # when I click on 'Login'
+    within 'nav' do
+      click_on 'Login', exact: true
+    end
     # and submit my email and password
     fill_in 'Email', with: account.email
     fill_in 'Password', with: account.password
@@ -20,9 +24,13 @@ feature 'Session' do
     # given I am a signed-in user
     account = create(:account)
     sign_in account, scope: :account
+    # and I am on the homepage
+    visit '/'
 
-    # when I visit the logout page
-    visit '/logout'
+    # when I click on 'Logout'
+    within 'nav' do
+      click_on 'Logout'
+    end
 
     # then I should be signed out
     expect(page).to have_text 'Signed out successfully'
