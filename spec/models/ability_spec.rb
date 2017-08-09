34ef2fc48054cb79f0af3078954630671f49f15c
Finn Woelm <public@finnwoelm.com>
2017-08-09 22:16:41 +0700

    Install gem 'CanCanCan' for user authorization
    Use CanCanCan to define user permissions for controller actions, such
    as edit and destroy.

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'cancan/matchers'
+
+RSpec.shared_examples 'having authorization' do |object_actions|
+  object_actions.each do |action|
+    it { is_expected.to be_able_to action, object }
+  end
+end
+
+RSpec.shared_examples 'not having authorization' do |object_actions|
+  object_actions.each do |action|
+    it { is_expected.not_to be_able_to action, object }
+  end
+end
+
+RSpec.describe Ability, type: :model do
+  subject(:ability) { Ability.new(user) }
+  let(:user) { create(:user) }
+
+  context 'Users' do
+    actions = %i[manage]
+    let(:object) { build_stubbed(:user) }
+
+    context 'when user is self' do
+      before { object.id = user.id }
+      it_should_behave_like 'having authorization', actions
+    end
+
+    context 'when user is not self' do
+      before { object.id = build_stubbed(:user).id }
+      it_should_behave_like 'not having authorization', actions
+    end
+  end
+end
