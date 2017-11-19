e3b084b86b56a03eb683b10b7ee5bfa376f107c6
Finn Woelm <public@finnwoelm.com>
2017-11-19 16:11:00 -0700

    Install gem 'google-api-client' (v0.17.1)
    A Ruby wrapper for various Google APIs, such as the Google Drive API.

---
+++ config/initializers/google_drive.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'google/apis/drive_v3'
+
+# Initialize Google Drive Service API if in development or production
+if Rails.env.development? || Rails.env.production?
+  require 'googleauth'
+  require 'googleauth/stores/file_token_store'
+end
