05c74f616fe5193503e777e41b393bf9ac3278c7
Finn Woelm <public@finnwoelm.com>
2017-11-19 13:30:40 -0700

    Limit color scheme to indigo
    Limit color scheme to single color until we implement custom colors.

--- app/models/color.rb
+++ app/models/color.rb
@@ -5,14 +5,17 @@ class Color
   class << self
     # define available color schemes
     def schemes
+      # [
+      #   { base: 'red',          text: 'white' },
+      #   { base: 'amber',        text: 'black' },
+      #   { base: 'yellow',       text: 'black' },
+      #   { base: 'light-green',  text: 'black' },
+      #   { base: 'light-blue',   text: 'white' },
+      #   { base: 'indigo',       text: 'white' },
+      #   { base: 'purple',       text: 'white' }
+      # ]
       [
-        { base: 'red',          text: 'white' },
-        { base: 'amber',        text: 'black' },
-        { base: 'yellow',       text: 'black' },
-        { base: 'light-green',  text: 'black' },
-        { base: 'light-blue',   text: 'white' },
-        { base: 'indigo',       text: 'white' },
-        { base: 'purple',       text: 'white' }
+        { base: 'indigo', text: 'white' }
       ]
     end
   end
