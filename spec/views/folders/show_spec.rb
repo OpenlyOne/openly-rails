c5be9b75db14bcaf2edb570f7bf1fe43c48bcb64
Finn Woelm <public@finnwoelm.com>
2017-11-21 21:49:10 -0700

    Add breadcrumbs to Folders#show
    When viewing a folder, show breadcrumbs of the folder's parents to make
    navigating through the folder hierarchy easier.

--- spec/views/folders/show_spec.rb
+++ spec/views/folders/show_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe 'folders/show', type: :view do
-  let(:project)           { create(:project) }
-  let(:folder)            { create(:file_items_folder, project: project) }
+  let(:folder)            { create(:file_items_folder, parent: nil) }
+  let(:project)           { folder.project }
   let(:files_and_folders) { files + folders }
-  let(:files)             do
+  let(:files) do
     create_list(:file_items_file, 5, project: project, parent: folder)
   end
-  let(:folders)           do
+  let(:folders) do
     create_list(:file_items_folder, 5, project: project, parent: folder)
   end
 
@@ -47,6 +47,30 @@ RSpec.describe 'folders/show', type: :view do
         href: profile_project_folder_path(
           project.owner, project.slug, folder.google_drive_id
         )
+      )
+    end
+  end
+
+  context 'when folder is not root' do
+    let(:folder) do
+      create :file_items_folder, project: parent.project, parent: parent
+    end
+    let(:parent) { create :file_items_folder }
+
+    it 'renders breadcrumbs' do
+      render
+      expect(rendered).to have_css 'nav'
+    end
+
+    it 'renders current folder' do
+      render
+      expect(rendered).to have_text folder.name
+    end
+
+    it 'renders link to home-folder breadcrumb' do
+      render
+      expect(rendered).to have_link(
+        '', href: profile_project_root_folder_path(project.owner, project)
       )
     end
   end
