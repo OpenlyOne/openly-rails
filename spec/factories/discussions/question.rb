6cf5fc28b1f54c590c0688fa4b3ef769121ce912
Finn Woelm <public@finnwoelm.com>
2017-11-21 23:22:56 -0700

    Remove discussions and replies
    Delete discussion- and reply-related files. For now, we are not using
    the discussion feature. Once we re- implement it, it will be quite
    different, so I decided to delete it for now.

--- spec/factories/discussions/question.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryGirl.define do
-  factory :discussions_question,
-          class: Discussions::Question,
-          parent: :discussions_base do
-  end
-end
--- spec/factories/replies.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryGirl.define do
-  factory :reply do
-    content { Faker::Lorem.paragraphs.join("\n\n") }
-    association :author, factory: :user
-    association :discussion, factory: :discussions_suggestion
-  end
-end
