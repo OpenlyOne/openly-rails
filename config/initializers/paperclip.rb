bc5dec44820a9322bee2336274a6e1aa84cd4627
Finn Woelm <public@finnwoelm.com>
2018-01-24 13:32:42 -0700

    Set up deployment settings for Paperclip
    Share public/systems between deployments and run
    `rake paperclip:refresh:missing_styles` on deploy.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -10,3 +10,7 @@ Paperclip.interpolates :attachment_url do |_attachment, _style|
   # Remove 'public' from the attachment storage path
   Settings.attachment_storage.sub(/^public/, '')
 end
+
+# Set the path for registering attachment styles
+Paperclip.registered_attachments_styles_path =
+  Rails.root.join(Settings.attachment_storage, 'paperclip_attachments.yml')
