f554d7d1a61fa7c83b82a0a06cbc2bf654941227
Finn Woelm <public@finnwoelm.com>
2018-01-28 15:30:52 -0700

    Display tags on project page
    User can see project tags on the projects#show page.

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :project do
     title { Faker::HitchhikersGuideToTheGalaxy.starship.first(50).strip }
     description     { Faker::Lorem.paragraph }
+    tags            { Faker::Lorem.words }
     sequence(:slug) { |n| "project-slug-#{n}" }
     owner           { build(:user) }
   end
