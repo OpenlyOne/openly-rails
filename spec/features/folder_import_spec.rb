970303e960c10cf37a5f2c1f6482f36651444f1c
Finn Woelm <public@finnwoelm.com>
2018-02-06 21:09:43 -0700

    Create live FolderImport and FileUpdate feature specs
    Rewrite FolderImport and FileUpdate specs to use the Google Drive API to
    trigger actual file actions on Google Drive.

--- spec/features/folder_import_spec.rb
+++ spec/features/folder_import_spec.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
-feature 'Folder Import' do
-  scenario 'Files are imported' do
-    mock_google_drive_requests if ENV['MOCK_GOOGLE_DRIVE_REQUESTS'] == 'true'
+feature 'Folder Import', :vcr do
+  let(:api_connection)  { Providers::GoogleDrive::ApiConnection.new(user_acct) }
+  let(:user_acct)       { ENV['GOOGLE_DRIVE_USER_ACCOUNT'] }
+  let(:tracking_acct)   { ENV['GOOGLE_DRIVE_TRACKING_ACCOUNT'] }
 
+  # create test folder
+  before { prepare_google_drive_test(api_connection) }
+
+  # delete test folder
+  after { tear_down_google_drive_test(api_connection) }
+
+  scenario 'Files are imported' do
     # given there is a project
     project = create(:project, title: 'My Awesome New Project!')
+
+    # and a Google Drive folder that contains three files
+    3.times do
+      create_google_drive_file(parent_id: google_drive_test_folder_id,
+                               api_connection: api_connection)
+    end
+
+    # and that folder is shared with the tracking account
+    api_connection.share_file(google_drive_test_folder_id, tracking_acct)
+
+    # and I wait 5 seconds for Google Drive to propagate the sharing settings to
+    # the folder's files
+    sleep 5 if VCR.current_cassette.recording?
 
     # and I am signed in as its owner
     account = project.owner.account
@@ -15,9 +36,11 @@ feature 'Folder Import' do
     visit "/#{project.owner.to_param}/#{project.to_param}/setup"
     # and fill in the Link to Google Drive Folder
     fill_in 'project_link_to_google_drive_folder',
-            with: Settings.google_drive_test_folder
+            with: 'https://drive.google.com/drive/folders/' \
+                  "#{google_drive_test_folder_id}"
     # and import
     click_on 'Import'
+
     # and go to files
     click_on 'Files'
 
