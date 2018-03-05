ebfbfd7d893333109d005377151cac7a698d2355
Finn Woelm <public@finnwoelm.com>
2018-03-05 15:38:16 -0700

    Fix Project: Delete staged root folder upon destroy
    Add dependent: :delete to staged_root_folder association, so that
    staged root folder is deleted correctly.

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,16 +11,18 @@ class Project < ApplicationRecord
                                           association_foreign_key: 'profile_id',
                                           validate: false
 
+  has_one :staged_root_folder,
+          -> { where is_root: true },
+          class_name: 'StagedFile',
+          dependent: :delete
+  has_one :root_folder, class_name: 'FileResource',
+                        through: :staged_root_folder,
+                        source: :file_resource
+
   has_many :staged_files, dependent: :destroy
   has_many :file_resources_in_stage, class_name: 'FileResource',
                                      through: :staged_files,
                                      source: :file_resource
-  has_one :staged_root_folder,
-          -> { where is_root: true },
-          class_name: 'StagedFile'
-  has_one :root_folder, class_name: 'FileResource',
-                        through: :staged_root_folder,
-                        source: :file_resource
 
   has_many :staged_non_root_files,
            -> { where is_root: false },
