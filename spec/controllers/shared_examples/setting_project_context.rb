700531a41cf5e81bdd75b499cc9982608f0fe785
Finn Woelm <public@finnwoelm.com>
2018-03-06 10:51:57 -0700

    Remove ProjectLockable and CanSetProjectContext (concerns)
    Remove no-longer needed controller concerns ProjectLockable and
    CanSetProjectContext. They were needed for Git-based infrastructure
    only.

--- spec/controllers/shared_examples/setting_project_context.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Expect the controller action to call #set_project and #set_project_context
-# methods required to correctly render the project head
-RSpec.shared_examples 'setting project context' do
-  after { run_request }
-  it    { is_expected.to receive(:set_project).and_call_original }
-  it    { is_expected.to receive(:set_project_context) }
-end
