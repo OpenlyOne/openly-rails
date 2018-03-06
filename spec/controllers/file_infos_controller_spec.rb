c4514dbbe4806ffee630d5026aaa2605fdd3dccb
Finn Woelm <public@finnwoelm.com>
2018-03-06 08:31:17 -0700

    FileInfosController: Replace Git- with DB-system
    Display files infos from database rather than Git infrastructure.

--- spec/controllers/file_infos_controller_spec.rb
+++ spec/controllers/file_infos_controller_spec.rb
@@ -1,34 +1,39 @@
 # frozen_string_literal: true
 
-require 'controllers/shared_examples/a_repository_locking_action.rb'
 require 'controllers/shared_examples/raise_404_if_non_existent.rb'
-require 'controllers/shared_examples/setting_project_context.rb'
 
 RSpec.describe FileInfosController, type: :controller do
-  let!(:file)     { create :file, parent: root }
-  let(:root)      { create :file, :root, repository: project.repository }
-  let!(:project)  { create :project }
+  let(:root)    { create :file_resource, :folder }
+  let(:folder)  { create :file_resource, :folder, parent: root }
+  let(:project) { create :project }
   let(:default_params) do
     {
       profile_handle: project.owner.to_param,
       project_slug:   project.slug,
-      id:             file.id
+      id:             folder.external_id
     }
   end
+
+  before { project.root_folder = root }
 
   describe 'GET #index' do
     let(:params)      { default_params }
     let(:run_request) { get :index, params: params }
 
-    include_examples 'raise 404 if non-existent', Profiles::Base
-    include_examples 'raise 404 if non-existent', Project
+    it_should_behave_like 'raise 404 if non-existent', Profiles::Base
+    it_should_behave_like 'raise 404 if non-existent', Project
     it_should_behave_like 'raise 404 if non-existent', nil do
       # when file does not exist and never has
       before { default_params[:id] = 'some-nonexistent-id' }
     end
 
-    it_should_behave_like 'a repository locking action'
-    it_should_behave_like 'setting project context'
+    context 'when id is of root folder' do
+      before      { params[:id] = root.external_id }
+
+      it 'raises a 404 error' do
+        expect { run_request }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
 
     it 'returns http success' do
       run_request
