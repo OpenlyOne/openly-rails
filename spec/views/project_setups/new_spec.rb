ad40df6591e81e80477f5c413d350ce5595ebbc6
Finn Woelm <public@finnwoelm.com>
2018-03-13 18:38:57 -0600

    ProjectSetupsController: Add views
    Add views for #new and #show to ProjectSetupsController.

--- spec/views/project_setups/new_spec.rb
+++ spec/views/project_setups/new_spec.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
 RSpec.describe 'project_setups/new', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:setup)   { build_stubbed(:project_setup) }
+  let(:project) { setup.project }
+
+  before { assign(:project, project) }
+  before { assign(:setup, setup) }
+
+  it 'renders a form with profile_project_setup_path action' do
+    render
+    expect(rendered).to have_css(
+      'form'\
+      "[action='#{profile_project_setup_path(project.owner, project)}']"\
+      "[method='post']"
+    )
+  end
+
+  it 'renders errors' do
+    setup.errors.add(:base, 'mock error')
+    render
+    expect(rendered).to have_css '.validation-errors', text: 'mock error'
+  end
+
+  it 'has an input field for link to Google Drive folder' do
+    render
+    expect(rendered).to have_css 'input#project_setup_link'
+  end
+
+  it 'has a button to import the folder' do
+    render
+    expect(rendered)
+      .to have_css "button[action='submit']", text: 'Import'
+  end
 end
