8ec5395425f3ea458d57b7877ec1c831483f892b
Finn Woelm <public@finnwoelm.com>
2018-04-25 08:39:02 -0600

    Administrate accounts: Skip blank password
    When updating accounts from the backend, skip the password field if it
    is left blank.

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -3,5 +3,18 @@
 module Admin
   # Administration controller for managing accounts
   class AccountsController < Admin::ApplicationController
+    before_action :ignore_blank_password, only: :update
+
+    private
+
+    # If password & password confirmation are both blank, assume that user did
+    # not want to update the password and remove the two from the params hash
+    def ignore_blank_password
+      return if params[:account][:password].present? ||
+                params[:account][:password_confirmation].present?
+
+      params[:account].delete(:password)
+      params[:account].delete(:password_confirmation)
+    end
   end
 end
