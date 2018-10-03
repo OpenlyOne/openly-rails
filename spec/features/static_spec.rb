f09faa39f14f763241c89b224e57b4f0712501aa
Finn Woelm <public@finnwoelm.com>
2018-10-03 15:36:12 -0600

    Landing Page: Target at Githubbers
    Replace target audience on landing page: From changemakers to Githubbers
    who work on documents.

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -3,7 +3,7 @@
 feature 'Static Pages' do
   scenario 'Visiting the home page' do
     visit '/'
-    expect(page).to have_content 'Create Change Together'
+    expect(page).to have_content 'GitHub for Google Drive'
     expect(page).to have_content 'Coming Soon'
   end
 end
