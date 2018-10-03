96f9577d5985472b8d230559bdb4035832028f34
Finn Woelm <public@finnwoelm.com>
2018-10-03 16:44:50 -0600

    Landing Page: Change target of CTA button
    Direct user to Google form for requesting early access upon clicking on
    call-to-action button.

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -4,6 +4,6 @@ feature 'Static Pages' do
   scenario 'Visiting the home page' do
     visit '/'
     expect(page).to have_content 'GitHub for Google Drive'
-    expect(page).to have_content 'Coming Soon'
+    expect(page).to have_content 'Request Early Access'
   end
 end
