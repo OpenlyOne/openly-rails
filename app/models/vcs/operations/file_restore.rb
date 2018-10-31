daea73b5d16b5081fab4b3576ff9587aca5fb012
Finn Woelm <public@finnwoelm.com>
2018-10-30 19:05:58 -0600

    Add VCS::Operations::FileRestore class
    The VCS::Operations::FileRestore class is used for restoring snapshots
    of files and is the foundation for both file and commit restoration.

---
+++ app/models/vcs/operations/file_restore.rb
@@ -0,0 +1,131 @@
+# frozen_string_literal: true
+
+module VCS
+  module Operations
+    # Restore a file snapshot to the provided target branch
+    class FileRestore
+      # Initialize a new instance of FileRestore and prepare for restoring the
+      # provided snapshot to the provided target_branch
+      def initialize(snapshot:, target_branch:)
+        self.snapshot = snapshot
+        self.target_branch = target_branch
+      end
+
+      # Perform the restoration of the snapshot
+      def restore
+        # Do nothing if there is no diff!
+        return unless diff.change?
+        raise 'This snapshot cannot be restored' unless restorable?
+
+        perform_restoration
+
+        # Update in stage
+        staged_file.update(snapshot_attributes.merge(external_id: external_id))
+      end
+
+      # The snapshot can only be restored if a backup is present OR
+      # if the restoration is only affecting location and name
+      def restorable?
+        snapshot.backup.present? || (!diff.addition? && !diff.modification?)
+      end
+
+      private
+
+      attr_accessor :snapshot, :target_branch
+      attr_writer :external_id
+
+      # Create remote file from backup copy
+      def add_file
+        replacement =
+          file_sync_class.new(snapshot.backup.external_id).duplicate(
+            name: snapshot.name,
+            parent_id: staged_parent.external_id
+          )
+        self.external_id = replacement.id
+      end
+
+      # Create remote file from backup copy and delete current file
+      def replace_file
+        add_file
+
+        # Delete file by removing it from its parent folder
+        # Calling the delete API endpoint results in insufficient permission
+        # error unless the action is performed by the file owner.
+        file_sync_class
+          .new(staged_file.external_id)
+          .relocate(to: nil, from: staged_file.parent.external_id)
+      end
+
+      # Move remote file
+      def relocate_file
+        file_sync_class
+          .new(staged_file.external_id)
+          .relocate(
+            to: staged_parent.external_id,
+            from: staged_file.parent.external_id
+          )
+      end
+
+      # Rename remote file
+      def rename_file
+        file_sync_class.new(staged_file.external_id).rename(snapshot.name)
+      end
+
+      # Calculate the diff of new snapshot vs currently staged snapshot
+      def diff
+        @diff ||=
+          VCS::FileDiff.new(
+            new_snapshot: snapshot,
+            old_snapshot: staged_file&.current_snapshot
+          )
+      end
+
+      def external_id
+        @external_id ||= staged_file.external_id
+      end
+
+      def file_sync_class
+        Providers::GoogleDrive::FileSync
+      end
+
+      def perform_restoration
+        # Add file
+        return add_file if diff.addition?
+
+        # # Replace file
+        return replace_file if diff.modification?
+
+        # # Move/rename file
+        relocate_file if diff.movement?
+        rename_file if diff.rename?
+      end
+
+      def staged_parent_of_snapshot_to_restore
+        target_branch
+          .staged_files
+          .joins(:current_snapshot)
+          .find_by(file_record_id: snapshot.file_record_parent_id)
+      end
+
+      def staged_parent
+        staged_parent_of_snapshot_to_restore || target_branch.root
+      end
+
+      def snapshot_attributes
+        snapshot
+          .attributes
+          .symbolize_keys
+          .slice(:name, :content_version, :mime_type)
+          .merge(parent: staged_parent)
+      end
+
+      # Identify the StagedFile to modify with the provided snapshot
+      def staged_file
+        @staged_file ||=
+          target_branch
+          .staged_files
+          .find_by(file_record_id: snapshot.file_record_id)
+      end
+    end
+  end
+end
---
+++ spec/integrations/vcs/operations/file_restore_spec.rb
@@ -0,0 +1,180 @@
+# frozen_string_literal: true
+
+RSpec.describe VCS::Operations::FileRestore, type: :model, vcr: true do
+  before  { prepare_google_drive_test }
+  after   { tear_down_google_drive_test }
+
+  describe '#restore' do
+    subject(:file_restore) do
+      described_class
+        .new(snapshot: snapshot_to_restore, target_branch: root.branch)
+    end
+
+    let!(:remote_subfolder) do
+      file_sync_class.create(
+        name: 'Folder',
+        parent_id: google_drive_test_folder_id,
+        mime_type: Providers::GoogleDrive::MimeType.folder
+      )
+    end
+
+    let!(:remote_file) do
+      file_sync_class.create(
+        name: 'original name',
+        parent_id: remote_subfolder.id,
+        mime_type: Providers::GoogleDrive::MimeType.document
+      )
+    end
+
+    let(:file_sync_class) { Providers::GoogleDrive::FileSync }
+
+    let!(:project)  { create(:project, owner_account_email: user_acct) }
+    let(:user_acct) { ENV['GOOGLE_DRIVE_USER_ACCOUNT'] }
+
+    let!(:root) do
+      create :vcs_staged_file, :root,
+             external_id: google_drive_test_folder_id,
+             branch: project.master_branch
+    end
+
+    let!(:subfolder) do
+      create :vcs_staged_file, :folder,
+             parent: root, external_id: remote_subfolder.id
+    end
+
+    let!(:file) do
+      build(:vcs_staged_file, external_id: remote_file.id, branch: root.branch)
+    end
+
+    let(:file_change) do
+      VCS::FileDiff.new(
+        new_snapshot: file.current_snapshot,
+        old_snapshot: snapshot_before_performing_restoration
+      )
+    end
+    let(:attributes_of_snapshot_to_restore) do
+      snapshot_to_restore
+        .attributes
+        .symbolize_keys
+        .slice(:name, :content_version, :file_record, :file_record_parent_id)
+    end
+
+    let(:snapshot_before_performing_restoration)  { file.current_snapshot }
+    let(:snapshot_to_restore)                     { file.current_snapshot }
+    let(:remote_file_after_restore) { file_sync_class.new(file.external_id) }
+    let(:parent_of_snapshot_to_restore) do
+      root.branch
+          .staged_files
+          .find_by(file_record_id: snapshot_to_restore.file_record_parent_id)
+    end
+    let(:expected_parent)           { parent_of_snapshot_to_restore }
+    let(:expected_content_version)  { snapshot_to_restore.content_version }
+
+    before do
+      # capture the initial snapshot which we will later restore
+      file.pull
+      snapshot_to_restore
+
+      # perform file actions, such as rename etc. & capture snapshot before
+      # restoration
+      file_actions
+      file.reload
+      file.pull
+      snapshot_before_performing_restoration
+
+      # perform the restoration
+      file_restore.restore
+      file.reload
+      file.pull
+    end
+
+    after do
+      expect(file).to have_attributes(
+        file_record_id: snapshot_to_restore.file_record_id,
+        name: snapshot_to_restore.name,
+        file_record_parent_id: expected_parent.file_record_id,
+        content_version: expected_content_version
+      )
+      expect(remote_file_after_restore).to have_attributes(
+        id: file.external_id,
+        name: snapshot_to_restore.name,
+        parent_id: expected_parent.external_id,
+        content_version: expected_content_version
+      )
+    end
+
+    context "when restoring the file's current snapshot" do
+      let(:file_actions) { nil }
+
+      it 'does not make any changes' do
+        expect(file_change).not_to be_change
+      end
+    end
+
+    context 'when file to restore does not exist in stage' do
+      let(:file_actions) { remote_file.delete }
+
+      it 'is added' do
+        expect(file_change).to be_addition
+      end
+    end
+
+    context 'when file is in a different location than the snapshot was' do
+      let(:remote_subfolder2) do
+        file_sync_class.create(
+          name: 'Subfolder 2',
+          parent_id: google_drive_test_folder_id,
+          mime_type: Providers::GoogleDrive::MimeType.folder
+        )
+      end
+      let(:subfolder2) do
+        create :vcs_staged_file, :folder,
+               parent: root, external_id: remote_subfolder2.id
+      end
+      let(:file_actions) do
+        remote_subfolder2
+        subfolder2
+        remote_file
+          .relocate(to: remote_subfolder2.id, from: remote_subfolder.id)
+        post_relocation_hook if defined?(post_relocation_hook)
+      end
+
+      it 'moves the file' do
+        expect(file_change).to be_movement
+      end
+
+      context 'when parent of snapshot to restore does not exist' do
+        let(:post_relocation_hook) do
+          remote_subfolder.delete
+          file.reload.tap(&:pull)
+          subfolder.reload.pull
+        end
+        let(:expected_parent) { root }
+
+        it 'moves snapshot to home folder' do
+          expect(file_change).to be_movement
+        end
+      end
+    end
+
+    context 'when file title differs from snapshot title' do
+      let(:file_actions) { remote_file.rename('new name') }
+
+      it 'renames the file' do
+        expect(file_change).to be_rename
+      end
+    end
+
+    context 'when file content differs from snapshot content' do
+      let(:file_actions) { remote_file.update_content('new content') }
+      let(:expected_content_version) do
+        remote_file_after_restore.content_version
+      end
+
+      it 'is modifies the file' do
+        expect(file_change).to be_modification
+        expect(file.external_id).not_to eq remote_file.id
+      end
+    end
+  end
+end
