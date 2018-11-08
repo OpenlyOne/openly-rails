383cc3c4c77d0dbe944e3aee63fb8ab9aedf5487
Finn Woelm <public@finnwoelm.com>
2018-11-08 10:57:55 -0700

    Add VCS::Operations::ContentDownloader for downloading remote files
    The VCS::Operations::ContentDownloader takes a remote file and downloads
    its contents as plain text.

---
+++ app/models/vcs/operations/content_downloader.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module VCS
+  module Operations
+    # Download content from remote file
+    class ContentDownloader
+      # Attributes
+      attr_accessor :remote_file_id
+
+      def initialize(remote_file_id:)
+        self.remote_file_id = remote_file_id
+      end
+
+      # Return the plain text version of the downloaded file
+      def plain_text
+        @plain_text ||= parser.text.strip
+      end
+
+      # Close the tempfile
+      def done
+        tempfile_for_download.close!
+      end
+
+      private
+
+      def remote_file
+        @remote_file ||= Providers::GoogleDrive::FileSync.new(remote_file_id)
+      end
+
+      def parser
+        @parser ||= Henkei.new(downloaded_file.tap(&:rewind))
+      end
+
+      # Return downloaded file. If not yet downloaded, download.
+      def downloaded_file
+        return @downloaded_file if @downloaded_file.present?
+
+        download_remote_file
+        @downloaded_file = tempfile_for_download
+      end
+
+      def download_remote_file
+        remote_file.download(destination: tempfile_for_download)
+      end
+
+      # TODO: Move tempfile handling to FileSync#download method and have
+      # =>    tempfile be returned from #download method rather than being
+      # =>    required as an argument
+      def tempfile_for_download
+        @tempfile_for_download ||= Tempfile.new.tap(&:binmode)
+      end
+    end
+  end
+end
