70b9000bad6cac4b3d654037144fbfbd926ffc45
Finn Woelm <public@finnwoelm.com>
2018-11-08 18:17:16 -0700

    Add VCS::Operations::ContentChange for diffing file contents
    VCS::Operations::ContentChange uses the shell command `dwdiff` to diff
    two texts against each other. The VCS::Operations::ContentChangeFragment
    class is used to represent fragments of content change: An addition, a
    deletion, or an unchanged text part.

---
+++ spec/integrations/vcs/operations/content_differ_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+RSpec.describe VCS::Operations::ContentDiffer, type: :model do
+  subject(:differ) do
+    described_class.new(new_content: new_content, old_content: old_content)
+  end
+
+  let(:new_content) { 'Hello, my name is Lara.' }
+  let(:old_content) { 'Hi, my last name is Long.' }
+
+  describe '.change(new_content, old_content)' do
+    subject(:change) { described_class.change(new_content, old_content) }
+
+    it do
+      is_expected.to eq(
+        '{--Hi,--}{++Hello,++} my {--last--} name is {--Long.--} {++Lara.++}'
+      )
+    end
+
+    context 'when text contains delimiter characters' do
+      let(:new_content) { '{++test++}' }
+      let(:old_content) { '{++test++}' }
+
+      it 'encodes them' do
+        is_expected.not_to include('{++')
+        is_expected.not_to include('++}')
+      end
+    end
+  end
+
+  describe '#fragments' do
+    subject(:fragments) { differ.fragments }
+
+    it 'fragments the change' do
+      expect(fragments.length).to eq 7
+      expect(fragments.map(&:fragment)).to eq(
+        ['{--Hi,--}', '{++Hello,++}', ' my ', '{--last--}', ' name is ',
+         '{--Long.--}', '{++Lara.++}']
+      )
+    end
+  end
+end
