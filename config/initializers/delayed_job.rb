767144e4a3996d52623a22b0275d0b5bc61e9eee
Finn Woelm <public@finnwoelm.com>
2018-11-10 14:03:25 -0700

    DelayedJob: Log output to a dedicated log file
    Log delayed job output to log/delayed_job_production.log, so that we can
    more easily debug delayed job vs Rails app.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -7,6 +7,10 @@ Rails.application.config.active_job.queue_adapter = :delayed_job
 require "#{Rails.root}/app/jobs/plugins/model_reference_plugin.rb"
 Delayed::Worker.plugins << ModelReferencePlugin
 
+# Log SQL queries to the delayed log
+require "#{Rails.root}/app/jobs/plugins/delayed_log_plugin.rb"
+Delayed::Worker.plugins << DelayedJobLogSetup
+
 # Do not destroy failed jobs (so we can figure out what went wrong and re-run
 # them)
 Delayed::Worker.destroy_failed_jobs = false
@@ -17,3 +21,7 @@ Delayed::Worker.max_run_time = 15.minutes
 # Delay jobs - unless we are in the test environment
 Delayed::Worker.delay_jobs = true
 Delayed::Worker.delay_jobs = false if Rails.env.test?
+
+# Set up logging for delayed job
+logfile = Rails.root.join('log', "delayed_job_#{Rails.env}.log")
+Delayed::Worker.logger = Logger.new(logfile)
