150c3cc036598654aaea1e40bb57c742c14266ba
Finn Woelm <public@finnwoelm.com>
2019-02-12 13:46:32 +0800

    Admin Panel: Allow setting is_premium attribute on accounts
    Admins can mark accounts as premium via the admin panel.

--- spec/features/admin_panel_spec.rb
+++ spec/features/admin_panel_spec.rb
@@ -24,6 +24,28 @@ feature 'Admin Panel' do
     click_on 'Projects'
     click_on 'Profiles/Users'
   end
+
+  scenario 'Accounts created default to non-premium' do
+    admin = create :account, :admin
+    sign_in_as admin
+
+    # when I create a new account
+    visit '/admin'
+    click_on 'Accounts'
+    click_on 'New account'
+    fill_in 'Email', with: 'test@user.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Name', with: 'Test User'
+    fill_in 'Handle', with: 'testuser'
+    click_on 'Create Account'
+
+    # then the account should be created
+    account = Account.find_by_email('test@user.com')
+    expect(account).to be_present
+    # and it should not be premium
+    expect(account).not_to be_premium
+  end
 end
 
 feature 'Analytics Dashboard' do
