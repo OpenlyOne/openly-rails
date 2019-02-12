895c5afd8c7fd73bfc1927f036688f969fa310c9
Finn Woelm <public@finnwoelm.com>
2019-02-12 18:21:59 +0800

    New project page: Add visibility radio buttons
    When creating a new project, users can set their project's visibility as
    either public or private.

--- spec/views/projects/new_spec.rb
+++ spec/views/projects/new_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe 'projects/new', type: :view do
-  let(:project) { build(:project) }
+  let(:project) { build(:project, is_public: nil) }
 
   before { assign(:project, project) }
 
@@ -25,14 +25,32 @@ RSpec.describe 'projects/new', type: :view do
     expect(rendered).to have_css 'input#project_title'
   end
 
-  it 'informs user that projects are private' do
+  it 'has a select option for visibility' do
     render
-    expect(rendered).to have_text 'Projects are private'
+    expect(rendered).to have_field(
+      class: 'public', type: 'radio', with: true, checked: false
+    )
+    expect(rendered).to have_field(
+      class: 'private', type: 'radio', with: false, checked: false
+    )
+  end
+
+  xit 'has the private option disabled' do
+    expect(rendered).to have_field('Private', type: 'select', disabled: true)
   end
 
   it 'has a button to create the project' do
     render
     expect(rendered)
       .to have_css "button[action='submit']", text: 'Create'
+  end
+
+  xcontext 'when user can create private projects' do
+    before { assign(:user_can_create_private_projects, true) }
+
+    it 'has the private option enabled' do
+      render
+      expect(rendered).to have_field('Private', type: 'select', disabled: false)
+    end
   end
 end
